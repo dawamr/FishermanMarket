4d8a2c5fe9da81f520591750b416ee91edfbeec3
Dawam Raja <akunnyadawam9@gmail.com>
2019-10-01 09:52:17 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Masuk</div>
+                <div class="panel-heading">Masuk Portal Nelayan</div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
